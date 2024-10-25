c0c1b7d102ca16c2d34ac7ac3272e8ffee9167af
arye shustimov <arye_s@optimove.com>
2024-10-26 00:17:00 +0300

    fix last movements and balance

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,25 @@ import { setTimeout } from "timers/promises";
 import loginService from './login.service'
 import lastMovementsService from './services/lastMovements.service';
 import balanceService from './services/balance.service'
+import creditCardService from './services/creditCard.service'
+import { Balance } from './types/balance';
 
 (async () => {
 
-    const browser = await loginService.createBrowser();
-    const page = await loginService.createPage(browser);
-    await setTimeout(5000);
-    await loginService.doLogin(page);
-    await setTimeout(5000);
-    const balance = await balanceService.scrap(page);
-    // const lastMovements = await lastMovementsService.scrap(page);
-    await setTimeout(5000);
+  const browser = await loginService.createBrowser();
+  const page = await loginService.createPage(browser);
+  await setTimeout(5000);
+  await loginService.doLogin(page);
+  await setTimeout(5000);
 
-    await setTimeout(20000);
-    // Close the browser
-    await browser.close();
+  // const balance: Balance = await balanceService.scrap(page);
+
+  // const lastMovements = await lastMovementsService.scrap(page);
+
+  // const creditCardMovements = await creditCardService.scrap(page);
+  await setTimeout(10000);
+
+  // await setTimeout(20000);
+  // Close the browser
+  await browser.close();
 })();
